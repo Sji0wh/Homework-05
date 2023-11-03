454259e33f636ed21db7158dae03c96c8ae7a2c0
Mark Kozlov <mark@indicosys.com>
2023-11-03 19:04:24 +0300

    commit

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
+
     @Test
     public void testChangeAgeSuccessful() {
         Person firstPerson = new Person(25, "Bob");
         firstPerson.changeName("Mart");
         assertEquals("Mart", firstPerson.getName());
     }
+
     @Test
     public void testCelebrateBirthday () {
         Person secondPerson = new Person(35, "Charlie");
