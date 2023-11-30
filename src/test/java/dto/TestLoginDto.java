025bc1e6a9a73420a033c65173bfd0dd4f6f4945
SJI0w <markkv93@gmail.com>
2023-11-30 19:38:28 +0200

    Lesson/HW 14 upload

---
+++ src/test/java/dto/TestLoginDto.java
@@ -0,0 +1,12 @@
+package dto;
+
+public class TestLoginDto {
+    private String username;
+
+    private String password;
+
+    public TestLoginDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/test/java/org/example/LoginTest.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import Helper.SetupFunctions;
+import com.google.gson.Gson;
+import dto.TestLoginDto;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class LoginTest {
+
+    static SetupFunctions setupFunctions;
+
+    @BeforeAll
+    public static void setup(){
+        setupFunctions = new SetupFunctions();
+
+    }
+
+    @Test
+    public void userLogin (){
+
+        //DTO creation
+        TestLoginDto logIn = new TestLoginDto(setupFunctions.getUsername(), setupFunctions.getPassword());
+
+        //Serialization
+        String testLoginDtoAsJson = new Gson().toJson(logIn);
+
+        //Api test
+        given()
+                .header("content-type", "application/json")
+                .body(testLoginDtoAsJson)
+                .post(setupFunctions.getBaseUrl() + "/login/student")
+                .then()
+                .log()
+                .body()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void incorrectCredentialsUserLogin (){
+
+        //DTO creation
+        TestLoginDto logIn = new TestLoginDto(setupFunctions.getUsername(), "123");
+
+        //Serialization
+        String testLoginDtoAsJson = new Gson().toJson(logIn);
+
+        //Api test
+        given()
+                .header("content-type", "application/json")
+                .body(testLoginDtoAsJson)
+                .post(setupFunctions.getBaseUrl() + "/login/student")
+                .then()
+                .log()
+                .body()
+                .assertThat()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
+}
